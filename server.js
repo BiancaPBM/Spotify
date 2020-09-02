c5daea8bbaba51c3a9ab6377180c1438f1b70f3b
BiancaPBM <pantarubianca@yahoo.com>
2020-09-02 21:46:53 +0300

    deploy heroku

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+
 const app = express();
 
-app.use(express.static('./dist/music-search-app.json'));
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/music-search-app/'}
-);
+app.use(express.static('./dist/music-search-app'));
+
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/music-search-app' }
+  );
 });
+
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
